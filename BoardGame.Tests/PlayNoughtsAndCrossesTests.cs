2cd23b914d23e125c3111cd74bbe9d0132aa3e37
cookedm87 <34936174+cookedm87@users.noreply.github.com>
2021-05-31 19:21:24 +0100

    Add files via upload

---
+++ BoardGame.Tests/PlayNoughtsAndCrossesTests.cs
@@ -0,0 +1,36 @@
+﻿using BoardGame.Library.Abstract;
+using BoardGame.Library.Concrete;
+using BoardGame.Library.Concrete.NoughtsAndCrosses;
+using NUnit.Framework;
+using System;
+
+
+namespace BoardGame.Tests
+{
+    class PlayNoughtsAndCrossesTests
+    {
+        NoughtsAndCrossesGame Game;
+        int BoardSize;
+
+        [SetUp]
+        public void Setup()
+        {
+            Game = new NoughtsAndCrossesGame(new NoughtsAndCrossesBoard(), new ConsoleWriter());
+            BoardSize = new NoughtsAndCrossesBoard().Size;
+        }
+
+        [Test]
+        public void PlayTurn_ShouldBeWithinBoardSize()
+        {
+            IMove move = Game.Players[0].PlayTurn();
+            Assert.Less(move.XPosition, BoardSize);
+            Assert.Less(move.YPosition, BoardSize);
+            move = Game.Players[0].PlayTurn();
+            Assert.Less(move.XPosition, BoardSize);
+            Assert.Less(move.YPosition, BoardSize);
+            move = Game.Players[0].PlayTurn();
+            Assert.Less(move.XPosition, BoardSize);
+            Assert.Less(move.YPosition, BoardSize);
+        }
+    }
+}
